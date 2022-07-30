e031219224727c01ce320db8789315bc2dc7defc
Naeem Shikalgar <naeems.9529414767@gmail.com>
2022-07-30 23:02:31 +0530

    first Commit

---
+++ app.js
@@ -0,0 +1,91 @@
+require('dotenv').config()
+
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose  = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const PORT = 3000|| process.env.PORT;
+
+const app = express();
+
+
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true,
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
+
+const userShema = mongoose.Schema({
+    email: String,
+    password: String,
+});
+
+const secret = ' thisissomesecretekeyfortheencryption';
+userShema.plugin(encrypt,{secret: process.env.SECRETE, encryptedFields:["password"]});
+
+const User = mongoose.model('User', userShema);
+
+
+
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.get('/login', function(req, res){
+    res.render('login')
+});
+
+app.get('/register', function(req,res){
+    res.render('register');
+});
+
+app.post('/register', function(req,res){
+   
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password,
+    });
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render('secrets')
+        }
+    })
+});
+
+app.post('/login', function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, function(err, foundUsers){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUsers){
+                if(foundUsers.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log(`Server successfully started on port ${PORT}`);
+});
+
